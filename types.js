894d924431c22c38c720c755e5f96c4e6176ac7c
Widian <evegenz@gmail.com>
2020-07-27 10:03:41 +0300

    methods

--- types.js
+++ types.js
@@ -11,9 +11,10 @@ alert(sum);
 
 function readNumber() {
     let a;
-    while ( !isFinite(a) ) {
+    do {
         a = prompt("Введите число:");
-    }
+    } while ( !isFinite(a) );
+
     if ( a === null || a === "" ) {
         return null;
     } else {
@@ -74,3 +75,97 @@ function extractCurrencyValue(str) {
 //-----------------------------------------------------------------------------
 
 
+// 4. т.к. массив это обьект и "копирование" его в переменную сделает только новую ссылку на него, а не новый массив.
+
+//-
+
+let styles = ['Джаз', 'Блюз'];
+
+styles.push('Рок-н-ролл');
+
+let arrLength = styles.length;
+
+styles[Math.floor((arrLength-1)/2)] = 'Классика';
+
+alert(styles.shift());
+
+styles.unshift('Рэп', 'Регги');
+
+//-
+
+// ? 
+
+//-
+
+function sumInput() {
+    let numbers = [];
+    while (true) { 
+        let val = prompt('Введите число.');
+        if ( !isFinite(val) || val === '' || val === null ) break;
+        numbers.push(+val);
+    } 
+
+    let sum = 0;
+    for (let number of numbers) {
+        sum += number;
+    }
+    return sum;
+}
+sumInput();
+
+//-
+
+function filterRange(arr, a, b) {
+    return arr.filter(item => (a <= item && item <= b));
+}
+  
+let arr = [5, 3, 8, 1];
+  
+let filtered = filterRange(arr, 1, 4);
+  
+alert( filtered ); // 3,1 (совпадающие значения)
+  
+alert( arr ); // 5,3,8,1 (без изменений)
+
+//-
+
+
+
+//-
+
+function filterRangeInPlace(arr, a, b) {
+    for (let i = 0; i < arr.length; i++) {
+        let value = arr[i];
+        if (value < a || value > b) {
+            arr.splice(i, 1);
+            i--;
+        }
+    }
+}
+  
+let arr = [5, 3, 8, 1];
+  
+filterRangeInPlace(arr, 1, 4); // удалены числа вне диапазона 1..4
+  
+alert( arr ); // [3, 1]
+
+//-
+
+let arr = [5, 2, 1, -10, 8];
+
+arr.sort((a, b) => b - a);
+
+alert( arr );
+
+//-
+
+function copySorted(arr) {
+    return arr.slice().sort();
+}
+  
+let arr = ["HTML", "JavaScript", "CSS"];
+  
+let sorted = copySorted(arr);
+  
+alert( sorted );
+alert( arr );
