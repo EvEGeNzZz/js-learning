642108648990efd039fe83a32a19ac65f2836352
Widian <evegenz@gmail.com>
2020-07-30 16:14:16 +0300

    dates2

--- types.js
+++ types.js
@@ -323,3 +323,25 @@ function topSalary(salaries) {
     }
     return topName;
 }
+
+
+//-----------------------------------------------------------------------------
+
+
+let date = new Date(2013, 1, 20, 3, 12);
+
+alert(date);
+
+//-
+
+function getWeekDay(date) {
+    let days = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
+
+    return days[date.getDay()];
+}
+
+let date = new Date(2012, 0, 3);
+alert( getWeekDay(date) ); 
+
+//-
+
